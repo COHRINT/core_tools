9d865e6b433f017c748c5537b8e3b8071b3811c3
Ian Loefgren <ian.loefgren@colorado.edu>
2017-05-26 09:43:16 -0600

    created setup.py file for testing

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='CoreTools',
+        version='2.0',
+        description='Core Tools experimental testbed',
+        author='Ian Loefgren',
+        author_email='ian.loefgren@colorado.edu',
+        url='https://github.com/COHRINT/core_tools',
+        # packages=['core_tools.core','gui_tools',
+        #             'models','policies','policy_translator',
+        #             'observation_interface']
+     )
