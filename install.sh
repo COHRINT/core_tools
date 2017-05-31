2ecbc03c9f463a8fdc387efcacc8495fd91c500f
Ian Loefgren <ian.loefgren@colorado.edu>
2017-05-31 15:46:30 -0600

    squashed incremental commits for install script and minor modifications to run.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-PROJECT_NAME="Core Tools"
+PROJECT_NAME="Cops and Robots 2.0"
+FILE_NAME="cops_and_robots_2"
 
 echo ""
 echo "Welcome to the $PROJECT_NAME installer"
@@ -18,8 +19,9 @@ echo ""
 
 echo "Are you sure you want to install $PROJECT_NAME? [y/n]"
 read user_input
-while [ $user_input != "y" ]; do
-  if [ $user_input == "n" ]; then
+while [ "$user_input" != "y" ]; do
+  if [ "$user_input" == "n" ]; then
+    echo "Exiting installer..."
     exit 1
   fi
   echo "Please choose [y/n]"
@@ -28,13 +30,20 @@ done
 
 
 # Define system packages required and Python packages required
-SYSTEM_DEPENDENCIES=("software-properties-common"
-                      "python-pip")
+SYSTEM_DEPENDENCIES=("python-pip"
+                      "python-pyqt5")
 SYSTEM_DEPENDENCIES_NUM=${#SYSTEM_DEPENDENCIES[@]}
 
-PYTHON_DEPENDENCIES=("PyYAML"
-                      "scipy")
-PYTHON_DEPENDENCIES_NUM=${#PYTHON_DEPENDENCIES[@]}
+PYTHON_DEPENDENCIES=("--upgrade pip"
+                      "PyYAML"
+                      "scipy"
+                      "matplotlib"
+                      "shapely"
+                      "pandas"
+                      "pytest"
+                      "descartes"
+                      "pyserial")
+PYTHON_DEPENDENCIES_NUM=${#PYTHON_DEPENDENCIES[@]} # subtract 1 b/c --upgrade pip is counted as 2
 
 REPOSITORY_LINK=""
 
@@ -42,6 +51,10 @@ REPOSITORY_LINK=""
 count=1
 echo ""
 echo "Installing required system packages..."
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo apt-add-repository universe
+sudo apt-get update
 for pkg in ${SYSTEM_DEPENDENCIES[@]}; do
   echo ""
   echo "Attempting to install $pkg ($count of $SYSTEM_DEPENDENCIES_NUM)"
@@ -49,14 +62,42 @@ for pkg in ${SYSTEM_DEPENDENCIES[@]}; do
   count=$((count+1))
 done
 
+# install virtualenv  and create environment before installing python packages
+echo "Installing virtualenv..."
+sudo pip install virtualenv
+echo ""
+echo "Do you want to create a virtual environement for this project? (This is highly suggested)[y/n]"
+read create_env
+
+while [ "$create_env" != "y" ] && [ "$create_env" != "n" ]; do
+  echo "Please choose [y/n]"
+  read create_env
+done
+
+# create directory for virtual environment and then create environment
+if [ "$create_env" == "y" ]; then
+  env_name=$FILE_NAME
+  dir_name="$HOME/.virtualenvs"
+  if [ ! -d $dir_name ]; then
+    echo "Creating directory $dir_name"
+    mkdir $dir_name
+  fi
+  cd $dir_name
+  virtualenv --system-site-packages $env_name
+  echo "Sourcing environment..."
+  source $dir_name/$env_name/bin/activate
+  echo "Type the commmand 'source $dir_name/$env_name/bin/activate' to activate this environment."
+  echo "Type 'deactivate' to deactivate the environment"
+fi
+
 # Install required Python packages using pip
 count=1
 echo ""
 echo "Installing required Python packages..."
-for pkg in ${PYTHON_DEPENDENCIES[@]}; do
+for pkg in "${PYTHON_DEPENDENCIES[@]}"; do
   echo ""
   echo "Attempting to install $pkg ($count of $PYTHON_DEPENDENCIES_NUM)"
-  pip install $pkg
+  pip install --ignore-installed $pkg
   count=$((count+1))
 done
 
